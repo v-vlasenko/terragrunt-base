e5d052d2cbe41e62be2a7e645a915455bb56da4f
Heshercat <alfiiaqa@gmail.com>
2024-11-17 18:42:35 +0200

    Test dev example

--- modules/s3_bucket/s3_config.tf
+++ modules/s3_bucket/s3_config.tf
@@ -10,16 +10,6 @@ resource "aws_s3_bucket" "this" {
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
-  bucket = aws_s3_bucket.this.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = var.encryption ? "AES256" : null
-    }
-  }
-}
-
 output "bucket_id" {
   value = aws_s3_bucket.this.id
 }
